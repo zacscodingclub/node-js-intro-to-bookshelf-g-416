e1afeb704a7cdde5f06e030da0cd5df67d3cbcfc
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-05-15 16:12:34 +0300

    Add beginnings of blog server tests

---
+++ test/blog_server_test.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const _      = require('lodash');
+const expect = require('unexpected');
+
+// Set mode to testing.
+process.env.TESTING = true;
+
+const blog = require('../blog_server');
+
+let mockUser = {
+  name: 'Sally Low',
+  username: 'sally',
+  email: 'sally@example.org'
+};
+
+describe('Models', () => {
+
+  before(() => {
+    blog.up();
+  });
+
+  after(() => {
+    blog.tearDown();
+  });
+
+  it('necessary models exist', (done) => {
+    expect(blog.User, 'to be defined');
+    done();
+  });
+
+  it('User model can save a user', (done) => {
+    blog.User
+      .forge(mockUser)
+      .save()
+      .then((usr) => {
+        expect(usr.attributes, 'to have keys', [
+          'name',
+          'email',
+          'username',
+        ]);
+        expect(usr.get('name'), 'to be', mockUser.name);
+        expect(usr.get('email'), 'to be', mockUser.email);
+        expect(usr.get('username'), 'to be', mockUser.username);
+        done();
+      });
+  });
+
+});
