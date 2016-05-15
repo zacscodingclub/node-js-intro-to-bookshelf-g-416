b5b4e638e5abe81267f473d21a77ea6cacc8ecb9
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-05-15 17:43:48 +0300

    Add server test suite with create user test

---
+++ test/blog_server_test.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const expect  = require('unexpected');
+const request = require('supertest');
+const baseUrl = 'http://localhost:3000';
+
+// Set mode to testing.
+process.env.TESTING = true;
+
+const blog = require('../blog_server');
+
+let mockUser = {
+  name: 'Sally Low',
+  username: 'sally',
+  email: 'sally@example.org'
+};
+
+describe('Server', () => {
+
+  it('POST /user with user data returns new user', (done) => {
+    request(baseUrl)
+      .post('/user')
+      .send(mockUser)
+      .expect(200)
+      .end((err, resp) => {
+        if (err) done(err);
+        expect(resp.body, 'to have keys', [
+          'id',
+          'username',
+          'email',
+          'name',
+        ]);
+        expect(resp.body.name, 'to be', 'Sally Low');
+        expect(resp.body.username, 'to be', 'sally');
+        expect(resp.body.email, 'to be', 'sally@example.org');
+        done();
+      });
+  });
+
+});
