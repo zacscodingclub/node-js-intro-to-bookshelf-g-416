aa553d42d9e77d7252f0436eea5f855169cd4615
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-05-15 16:11:59 +0300

    Add package.json test

---
+++ test/package_json_test.js
@@ -0,0 +1,23 @@
+const expect = require('unexpected');
+const packageJson = require('../package.json');
+
+describe('package.json', () => {
+  it('has the correct dependencies', () => {
+    expect(packageJson.dependencies, 'to have keys',
+      'express',
+      'body-parser',
+      'knex',
+      'bookshelf'
+    );
+  });
+
+  it('has the correct devDependencies', () => {
+   expect(packageJson.devDependencies, 'to have keys',
+     'chai',
+     'mocha',
+     'mocha-multi',
+     'supertest',
+     'unexpected'
+   );
+  });
+});
