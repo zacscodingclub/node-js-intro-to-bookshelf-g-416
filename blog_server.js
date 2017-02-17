b3a78ede293778fe4024cec13e5896b2fe612c39
Chad Nance <chad@wncit.com>
2017-02-17 13:27:23 -0500

    update migrations + server

--- blog_server.js
+++ blog_server.js
@@ -16,8 +16,135 @@ const bookshelf = require('bookshelf')(knex);
 
 // This is a good place to start!
 
+const User = bookshelf.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+  posts: function() {
+    return this.hasMany(Posts, 'author');
+  },
+  comments: function() {
+    return this.hasMany(Comments);
+  }
+});
 
+const Posts = bookshelf.Model.extend({
+  tableName: 'posts',
+  hasTimestamps: true,
+  author: function() {
+    return this.belongsTo(User, 'author');
+  },
+  comments: function() {
+    return this.hasMany(Comments);
+  }
+});
 
+const Comments = bookshelf.Model.extend({
+  tableName: 'comments',
+  hasTimestamps: true,
+  user: function() {
+    return this.belongsTo(User);
+  },
+  post: function() {
+    return this.belongsTo(Posts);
+  }
+});
+
+exports.User = User;
+exports.Posts = Posts;
+exports.Comments = Comments;
+
+app.get('/user/:id', (req, res) => {
+  User
+    .forge({id: req.params.id})
+    .fetch()
+    .then((usr) => {
+      if(_.isEmpty(usr)) {
+        return res.sendStatus(404);
+      }
+      res.send(usr);
+    })
+    .catch(e => {
+      console.error(e);
+      return res.sendStatus(500);
+    });
+});
+
+app.post('/user', (req, res) => {
+  if(_.isEmpty(req.body)) {
+    return res.sendStatus(400);
+  }
+  User
+    .forge(req.body)
+    .save()
+    .then(usr => {
+      res.send({id: usr.id});
+    })
+    .catch(e => {
+      console.error(e);
+      return res.sendStatus(500);
+    })
+});
+
+app.get('/posts', (req, res) => {
+  Posts
+    .collection()
+    .fetch()
+    .then(posts => {
+      res.send(posts);
+    })
+    .catch(e => {
+      console.error(e);
+      return res.sendStatus(500);
+    });
+});
+
+app.get('/post/:id', (req, res) => {
+  Posts
+    .forge({id: req.params.id})
+    .fetch({withRelated: ['author', 'comments']})
+    .then((post) => {
+      if(_.isEmpty(post)) {
+        return res.sendStatus(404);
+      }
+      res.send(post);
+    })
+    .catch(e => {
+      console.error(e);
+      return res.sendStatus(500);
+    });
+});
+
+app.post('/post', (req, res) => {
+  if(_.isEmpty(req.body)) {
+    return res.sendStatus(400);
+  }
+  Posts
+    .forge(req.body)
+    .save()
+    .then(post => {
+      res.send({id: post.id});
+    })
+    .catch(e => {
+      console.error(e);
+      return res.sendStatus(500);
+    })
+});
+
+app.post('/comment', (req, res) => {
+  if(_.isEmpty(req.body)) {
+    return res.sendStatus(400);
+  }
+  Comments
+    .forge(req.body)
+    .save()
+    .then(post => {
+      res.send({id: post.id});
+    })
+    .catch(e => {
+      console.error(e);
+      return res.sendStatus(500);
+    })
+});
 
 // Exports for Server hoisting.
 const listen = (port) => {
